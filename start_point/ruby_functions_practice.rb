6c8491ebc3bc3d946f6917713362562ac99070f2
Michelle Austin <mchllaustin@gmail.com>
2018-03-06 16:54:38 +0000

    first commit

---
+++ start_point/ruby_functions_practice.rb
@@ -0,0 +1,53 @@
+def return_10
+  return 10
+end
+
+def add(first_number, second_number)
+  return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+  return first_number - second_number
+end
+
+def multiply(first_number, second_number)
+  return first_number * second_number
+end
+
+def divide(first_number, second_number)
+  return first_number / second_number
+end
+
+def length_of_string(string)
+  string = "A string of length 21".length
+  return string
+end
+
+def join_string(string_1, string_2)
+  string_1 = "Mary had a little lamb, "
+  string_2 = "it's fleece was white as snow"
+return string_1 + string_2
+end
+
+def add_string_as_number(first_number, second_number)
+return first_number.to_i + second_number.to_i
+end
+
+def number_to_full_name(month_string, full_month_name)
+first_month_string = "January"
+second_month_string = "February"
+third_month_string = "March"
+fourth_month_string = "April"
+fifth_month_string = "May"
+sixth_month_string = "June"
+seventh_month_string = "July"
+eighth_month_string = "August"
+ninth_month_string = "September"
+tenth_month_string = "October"
+eleventh_month_string = "November"
+twelfth_month_string = "December"
+
+
+
+
+end
